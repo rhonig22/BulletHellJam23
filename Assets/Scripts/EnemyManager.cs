96b21756b524d29ded780073d66e44d84af388ec
rhonig22 <ryan.honig22@outlook.com>
2023-05-04 14:23:40 -0700

    Enemy Movement

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,9 +7,11 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> enemies= new List<GameObject>();
+    [SerializeField] private GameObject player;
     [SerializeField] private BulletManager enemyBullets;
     private readonly float enemyWaitPeriod = 1f;
     private readonly float enemyYRange = 4f;
+    private readonly float bulletDelay = .05f;
 
     private void Start()
     {
@@ -27,13 +29,25 @@ public class EnemyManager : MonoBehaviour
     IEnumerator startEnemy(int index, float delay)
     {
         GameObject enemy = enemies[index];
+        EnemyCollider collider = enemy.GetComponentInChildren<EnemyCollider>();
+        collider.startFire.AddListener(() => { StartCoroutine(spawnBullets(enemy));  });
         while (true)
         {
             yield return new WaitForSeconds(enemyWaitPeriod + delay);
             delay = 4f;
             enemy.transform.position = new Vector3(0, Random.Range(-enemyYRange, enemyYRange), 0);
             enemy.GetComponentInChildren<Animator>().SetTrigger("Start");
-            enemyBullets.Spawn(enemy.transform);
+        }
+    }
+
+    IEnumerator spawnBullets(GameObject enemy)
+    {
+        EnemyCollider collider = enemy.GetComponentInChildren<EnemyCollider>();
+        while (!collider.isDead && collider.isActive)
+        {
+            yield return new WaitForSeconds(bulletDelay);
+            enemyBullets.Spawn(enemy.transform.GetChild(0));
+
         }
     }
 }
